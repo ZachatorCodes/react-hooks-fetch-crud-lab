d117da12188e20ad9767e27b04da034969450681
Zach Schwartz <zachatorcodes@gmail.com>
2023-02-10 15:39:08 -0700

    Finished lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteItem, onAnswerChange }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,39 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => onDeleteItem(question));
+  }
+
+  function handleAnswerChange(e) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        correctIndex: parseInt(e.target.value),
+      }),
+    })
+      .then((r) => r.json())
+      .then((data) => onAnswerChange(data));
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleAnswerChange}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questionList, setQuestionList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestionList(data));
+  }, []);
+
+  function handleDeleteItem(deletedItem) {
+    const updatedQuestions = questionList.filter(
+      (question) => question.id !== deletedItem.id
+    );
+    setQuestionList(updatedQuestions);
+  }
+
+  function handleChangeCorrectAnswer(updatedQuestion) {
+    const updatedQuestions = questionList.map((question) => {
+      if (question.id === updatedQuestion.id) {
+        return updatedQuestion;
+      } else {
+        return question;
+      }
+    });
+    setQuestionList(updatedQuestions);
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questionList.map((question) => (
+          <QuestionItem
+            question={question}
+            key={question.id}
+            onDeleteItem={handleDeleteItem}
+            onAnswerChange={handleChangeCorrectAnswer}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
